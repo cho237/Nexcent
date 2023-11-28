f4c2250098e87b3e051d1ca795866147abbe139c
choTejiz <kinsly.cho@tejiz.com>
2023-11-28 12:12:57 +0100

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import {NgOptimizedImage} from "@angular/common";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent
   ],
     imports: [
         BrowserModule,
